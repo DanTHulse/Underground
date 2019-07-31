29756111751712b368f35ccc6df6530f4c1bf506
Dan Hulse <dan.hulse@gastag.co.uk>
2019-07-31 15:57:15 +0100

    Added types

--- Underground/Program.fs
+++ Underground/Program.fs
@@ -1,6 +1,38 @@
 ﻿open System
 
+type Lines =
+    Northern = 0
+  | Circle = 1
+  | District = 2
+  | HammersmithCity = 3
+  | Metropolitan = 4
+  | Central = 5
+  | Jubilee = 6
+  | Bakerloo = 7
+  | Victoria = 8
+  | Piccadily = 9
+  | WaterlooCity = 10
+  | Elizabeth = 11
+  | DLR = 12
+  | Overground = 13
+  | Trams = 14
+  | AirLine = 15
+  | Walking = 16
+
+type Directions =
+    Northbound = 'n'
+  | Southbound = 's'
+  | Eastbound = 'e'
+  | Westbound = 'w'
+
+let square x = x * x;
+
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+    let num, numVal = Int32.TryParse(argv.[0]);
+    if num then
+        let sq = (square numVal);
+        printfn "%d squared is: %d!" numVal sq;
+    else
+        printfn "Could not parse argument: %s" argv.[0];
+    0;
