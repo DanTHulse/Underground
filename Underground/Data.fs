a2a2d29e6d0d1586cadaef71372f5c92b84810f4
Dan Hulse <dan.hulse@gastag.co.uk>
2019-08-02 14:57:37 +0100

    Added Data folder

--- Underground/Data.fs
+++ Underground/Data.fs
@@ -6,7 +6,7 @@ open FSharp.Data
 
 module Data =
     let loadData =
-        let value = JsonValue.Load(__SOURCE_DIRECTORY__ + "\\LU_Data.json").ToString()
+        let value = JsonValue.Load(__SOURCE_DIRECTORY__ + "\\Data\\LU_Data.json").ToString()
         JsonConvert.DeserializeObject<Station list>(value)
 
     let findStationById (id: int) =
