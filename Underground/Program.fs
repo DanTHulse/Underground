ebc317d5db733a65d455377619405b15a849dff5
Dan Hulse <dan.hulse@gastag.co.uk>
2019-08-06 16:54:42 +0100

    Starting main menu

--- Underground/Program.fs
+++ Underground/Program.fs
@@ -11,8 +11,28 @@ module Entry =
 
         printfn "\n Find the quickest route between the two stations:"
         printfn "\n %s --> %s" startStation.name endStation.name
+        printfn "\n You can catch the following trains from this station:"
 
-        let currentStation = startStation
+        let trains =
+            startStation.lines
+            |> List.collect(fun s -> s.stations |> List.map(fun m -> (s.line, m.branch)))
+        
+        trains
+        |> List.iteri(fun i (l, s) -> printfn "%d: %s - %s" i (l.ToString()) s)
 
-        printfn "Current Station: %s" currentStation.name
+        let chosenTrain =
+            Console.ReadLine()
+            |> Int32.Parse
+            |> fun s -> trains.[s]
+        
+        while true do
+            Console.Clear()
+            printfn "\n Find the quickest route between the two stations:"
+            printfn "\n %s --> %s" startStation.name endStation.name
+
+            let currentStation = startStation
+            let currentLine, currentTrain = chosenTrain
+
+            printfn "Current Station: %s" currentStation.name
+
         0;
