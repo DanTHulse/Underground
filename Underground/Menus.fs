db921abb71213bb01d4f79c366980d57dc7f18fc
Dan Hulse <dan.hulse@gastag.co.uk>
2019-08-12 16:08:26 +0100

    Finished migration of menus

--- Underground/Menus.fs
+++ Underground/Menus.fs
@@ -5,6 +5,29 @@ open StationData
 open TrainData
 
 module Menus =
+    let rec start () =
+        // let startStation = findRandomStation()
+        // let endStation = findRandomStation()
+        let startStation = findStationById(35)
+        let endStation = findStationById(270)
+
+        Console.Clear()
+        printfn "\n Find the quickest route between the two stations:"
+        printfn "\n %s --> %s" startStation.name endStation.name
+        printfn "\n Do you want to re-roll? (Y/N)\n"
+
+        match Console.ReadKey().Key with
+        | ConsoleKey.Y -> start ()
+        | _ -> (startStation, endStation)
+
+    let scoreDisplay (startS: Station, endS: Station, score: int) =
+        Console.Clear()
+        let minutes = score / 60
+        let seconds = score % 60
+
+        printfn "\n You made it from %s --> %s in:" startS.name endS.name
+        printfn " %dm and %ds" minutes seconds
+
     let interchangeDisplay (station: Station) =
         printfn " -> Change for %s" (findLinesForStation (station)
                                       |> List.map (fun f -> f.ToString())
@@ -23,18 +46,6 @@ module Menus =
         printfn "\n This station is: %s" currentS.name
         interchangeDisplay (currentS)
 
-    let rec start () =
-        let startStation = findRandomStation()
-        let endStation = findRandomStation()
-
-        printfn "\n Find the quickest route between the two stations:"
-        printfn "\n %s --> %s" startStation.name endStation.name
-        printfn "\n Do you want to re-roll? (Y/N)\n"
-
-        match Console.ReadKey().Key with
-        | ConsoleKey.Y -> start ()
-        | _ -> (startStation, endStation)
-
     let boardTrain (currentS: Station) =
         currentS
         |> findLinesForStation
@@ -46,14 +57,21 @@ module Menus =
         let (startS, endS) = start ()
         let mutable currentS = startS
         let mutable currentT = boardTrain (currentS)
+        let mutable totalCost = 0
 
         while currentS <> endS do
             Console.Clear()
             display (startS, endS, currentS, currentT)
 
-            let choice =
+            printfn "\n Do you want to stay on this train? (Y/n)"
+
+            let (nextT, lineCost) =
                 match Console.ReadKey().Key with
-                | ConsoleKey.N -> true
-                | _ -> false
-            ()
-        ()
+                | ConsoleKey.N -> (boardTrain (currentS), 120)
+                | _ -> (currentT, 0)
+
+            let (nextS, cost) = findNextStation (currentS, nextT)
+            currentS <- nextS
+            totalCost <- (totalCost + cost + lineCost)
+        
+        scoreDisplay (startS, endS, totalCost)
--- Underground/Program.fs
+++ Underground/Program.fs
@@ -6,19 +6,9 @@ module Entry =
     [<EntryPoint>]
     let main _ =
         let mutable mainLoop = true
-        let mutable score = 0
 
         while mainLoop do
-            let (startS, endS) = Menu.start()
-
-            let mutable currentS = startS
-
-            while currentS <> endS do
-                let (nextS, cost) = Menu.loadDisplay()
-                score <- score + cost
-                currentS <- nextS
-
-            Menu.scoreDisplay(score)
+            Menus.gameLoop()
 
             printfn "\n Do you want to play again? (Y/N)"                        
             mainLoop <-match Console.ReadKey().Key with
