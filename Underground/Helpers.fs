b414aeb15be6840fd53efc6e4608b2bf9f8aa96d
Dan Hulse <dan.hulse@gastag.co.uk>
2019-08-14 10:52:00 +0100

    removed old coloured console helpers

--- Underground/Helpers.fs
+++ Underground/Helpers.fs
@@ -7,15 +7,6 @@ open Colorful
 
 [<AutoOpen>]
 module Helpers =
-    let consoleColor (fc : ConsoleColor) = 
-        let current = Console.ForegroundColor
-        Console.ForegroundColor <- fc
-        { new IDisposable with
-              member x.Dispose() = Console.ForegroundColor <- current }
-    
-    let cprintf color str = Printf.kprintf (fun s -> use c = consoleColor color in printf "%s" s) str
-    let cprintfn color str = Printf.kprintf (fun s -> use c = consoleColor color in printfn "%s" s) str
-
     let enumToList<'a> = (Enum.GetValues(typeof<'a>) :?> ('a [])) |> Array.toList
 
     let shuffleList next xs = xs |> Seq.sortBy(fun _ -> next())
